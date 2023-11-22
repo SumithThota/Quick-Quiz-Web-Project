f2c3dbb09d5b63261299716978daa6debe7a681c
SumithThota <108344062+SumithThota@users.noreply.github.com>
2023-11-22 10:53:05 +0530

    Add files via upload
    The  “QUICK QUIZZ”project will be developed to overcome the time consuming problem of manual system. Apart from that in current system, checking the answer sheets after taking test, waste the examiners time, so this application will check the correct answer and save the examiner time and carry the examination in an effective manner. The users which are use this system don’t need to high computing knowledge

---
+++ Quick_Quiz/student/includes/footer.php
@@ -0,0 +1,4 @@
+﻿ <div style="   position: fixed;left: 0;bottom: 0;width: 100%;background-color: #37474f;color: white;text-align: center;padding-top:15px;">
+  
+<p>@ 2023 Quick Quiz | JNTUHCEJ | Department of Computer Science </p>
+</div>
